2f0b79abcdd80526037c2313471fcfa0c2bb5cc2
anvarunical <anvarunical@gmail.com>
2025-11-13 23:30:43 +0500

    Initial commit!

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Mini Twitter Mongo API')
+    .setDescription('Mini Twitter Mongo API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
